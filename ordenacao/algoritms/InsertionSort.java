5df4e02b74794fe8e01d094d606ae10e8a63bcb5
Ezequiel Henrique <ezequielhenrique806@gmail.com>
2023-03-07 20:49:07 +0000

    Insertion Sort

--- ordenacao/algoritms/InsertionSort.java
+++ ordenacao/algoritms/InsertionSort.java
@@ -7,8 +7,23 @@ public class InsertionSort implements Sorter{
     @Override
     public int[] sort(int[] elements) {
         int [] sorted = elements.clone();
-        //TODO Implementar
-        return sorted;
+
+        int aux;
+        int j;
+        
+        for (int i=1; i < elements.length; i++) {
+           aux = elements[i];
+           j = i-1;
+
+           while(j>= 0 && elements[j] > aux) {
+            elements[j+1] = elements[j];
+            j--;
+           }
+
+           elements[j+1] = aux;
+        }
+
+        return elements;
     }
 
     @Override
