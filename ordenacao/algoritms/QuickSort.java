46188c57b1b9c67f0ca6d0e6c3cf53ea06628d87
Ezequiel Henrique <ezequielhenrique806@gmail.com>
2023-03-17 23:46:33 +0000

    QuickSort

--- ordenacao/algoritms/QuickSort.java
+++ ordenacao/algoritms/QuickSort.java
@@ -7,8 +7,38 @@ public class QuickSort implements Sorter {
     @Override
     public int[] sort(int[] elements) {
         int [] sorted = elements.clone();
-        //TODO Implementar
+
+        quickSort(sorted, 0, sorted.length-1);
+        
         return sorted;
+    }
+
+    private void quickSort(int[] array, int start, int end) {
+        if (start < end) {
+            int pivoPos = partition(array, start, end);
+            quickSort(array, start, pivoPos-1);
+            quickSort(array, pivoPos+1, end);
+        }
+    }
+
+    private int partition(int[] array, int start, int end) {
+        int pivo = array[end];
+        int i = start;
+
+        for (int j=start; j<end; j++) {
+            if (array[j] <= pivo) {
+                int aux = array[i];
+                array[i] = array[j];
+                array[j] = aux;
+
+                i++;
+            }
+        }
+        int aux = array[i];
+        array[i] = array[end];
+        array[end] = aux;
+
+        return i;
     }
 
     @Override
